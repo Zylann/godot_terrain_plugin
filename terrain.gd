1832a48dec9f8933911684988445eeb9a0b8c7b1
Marc Gilleron <marc.gilleron@gmail.com>
2016-07-25 00:53:12 +0200

    Added ability to see the terrain's size in the editor + fixed size inconsistency

--- terrain.gd
+++ terrain.gd
@@ -1,25 +1,25 @@
-#tool
+tool
 extends Node
 
 const CHUNK_SIZE = 16
+const MAX_TERRAIN_SIZE = 1024
 
 class Chunk:
 	var mesh_instance = null
 	var pos = Vector2(0,0)
 
-# TODO Make this dynamic
-export(int) var terrain_size = 64
+export(int) var terrain_size = 64 setget set_terrain_size, get_terrain_size
 # TODO Make this dynamic
 export(Material) var material = null
 
-var _data = null
-var _normals = null
+var _data = []
+var _normals = []
 
 var _brush = null
 var _brush_sum = 0
 var _brush_radius = 6
 
-var _chunks = null
+var _chunks = []
 var _chunks_x = 0
 var _chunks_y = 0
 var _dirty_chunks = {}
@@ -27,37 +27,75 @@ var _dirty_chunks = {}
 
 func _ready():
 	set_process_input(true)
-	
-	_data = create_grid(terrain_size, terrain_size, 0.0)
-	_normals = create_grid(terrain_size, terrain_size, Vector3(0,1,0))
-	
-	_chunks_x = up_div(terrain_size, CHUNK_SIZE)
-	_chunks_y = up_div(terrain_size, CHUNK_SIZE)
-	_chunks = create_grid(_chunks_x, _chunks_y)
-	for y in range(_chunks.size()):
-		var row = _chunks[y]
-		for x in range(row.size()):
-			var chunk = Chunk.new()
-			chunk.mesh_instance = MeshInstance.new()
-			chunk.mesh_instance.set_name("chunk_" + str(x) + "_" + str(y))
-			chunk.mesh_instance.set_translation(Vector3(x,0,y) * CHUNK_SIZE)
-			if material != null:
-				chunk.mesh_instance.set_material_override(material)
-			chunk.pos = Vector2(x,y)
-			add_child(chunk.mesh_instance)
-			row[x] = chunk
-			update_chunk_at(x,y)
-	
+	_on_terrain_size_changed()
 	_generate_brush(_brush_radius)
 	#generate_terrain()
-	if not get_tree().is_editor_hint():
-		set_process(true)
+	#if not get_tree().is_editor_hint():
+	set_process(true)
+
+#func _debug_print_actual_size(grid, name):
+#	var actual_width = 0
+#	if grid.size() != 0:
+#		actual_width = grid[0].size()
+#	print("Actual size of " + name + ": " + str(grid.size()) + " x " + str(actual_width))
 
 
-static func up_div(a, b):
-	if a % b != 0:
-		return a / b + 1
-	return a / b
+func get_terrain_size():
+	return terrain_size
+
+func set_terrain_size(new_size):
+	if new_size != terrain_size and new_size > 0:
+		if new_size > MAX_TERRAIN_SIZE:
+			new_size = MAX_TERRAIN_SIZE
+			print("Max size reached, clamped at " + str(MAX_TERRAIN_SIZE) + " for your safety :p")
+		terrain_size = new_size
+		print("Setting terrain_size to " + str(terrain_size))
+		_on_terrain_size_changed()
+
+
+func _on_terrain_size_changed():
+	_chunks_x = up_div(terrain_size, CHUNK_SIZE)
+	_chunks_y = up_div(terrain_size, CHUNK_SIZE)
+	
+	if is_inside_tree():
+		
+		resize_grid(_data, terrain_size+1, terrain_size+1, 0)
+		resize_grid(_normals, terrain_size+1, terrain_size+1, Vector3(0,1,0))
+		resize_grid(_chunks, _chunks_x, _chunks_y, funcref(self, "_create_chunk_cb"), funcref(self, "_delete_chunk_cb"))
+		
+		for key in _dirty_chunks.keys():
+			if key.mesh_instance == null:
+				_dirty_chunks.erase(key)
+		
+		for y in range(0, _chunks.size()-1):
+			var row = _chunks[y]
+			_set_chunk_dirty(row[row.size()-1])
+		if _chunks.size() != 0:
+			var last_row = _chunks[_chunks.size()-1]
+			for x in range(0, last_row.size()):
+				_set_chunk_dirty(last_row[x])
+		
+		_update_dirty_chunks()
+
+
+func _delete_chunk_cb(chunk):
+	chunk.mesh_instance.queue_free()
+	chunk.mesh_instance = null
+
+
+func _create_chunk_cb(x, y):
+	#print("Creating chunk (" + str(x) + ", " + str(y) + ")")
+	var chunk = Chunk.new()
+	chunk.mesh_instance = MeshInstance.new()
+	chunk.mesh_instance.set_name("chunk_" + str(x) + "_" + str(y))
+	chunk.mesh_instance.set_translation(Vector3(x,0,y) * CHUNK_SIZE)
+	if material != null:
+		chunk.mesh_instance.set_material_override(material)
+	chunk.pos = Vector2(x,y)
+	add_child(chunk.mesh_instance)
+	_dirty_chunks[chunk] = true
+	#update_chunk(chunk)
+	return chunk
 
 
 func _generate_brush(radius):
@@ -67,7 +105,7 @@ func _generate_brush(radius):
 	for y in range(-radius, radius):
 		for x in range(-radius, radius):
 			var d = Vector2(x,y).distance_to(Vector2(0,0)) / float(radius)
-			var v = clamp(1.0 - d*d, 0.0, 1.0)
+			var v = clamp(1.0 - d*d*d, 0.0, 1.0)
 			_brush[y+radius][x+radius] = v
 			_brush_sum += v
 
@@ -126,33 +164,36 @@ func _set_area_dirty(tx, ty, radius):
 			if x >= 0 and y >= 0 and x < _chunks_x and y < _chunks_y:
 				_set_chunk_dirty_at(x, y)
 
-
 func _set_chunk_dirty_at(cx, cy):
-	var chunk = _chunks[cy][cx]
+	_set_chunk_dirty(_chunks[cy][cx])
+
+func _set_chunk_dirty(chunk):
 	_dirty_chunks[chunk] = true
 
 
 func _process(delta):
-	var camera = get_parent().get_node("Camera")
-	var origin = camera.get_translation()
-	var dir = camera.get_transform().basis * Vector3(0,0,-1)
-	var hit_pos = raycast(origin, dir)
-	if hit_pos != null:
-		get_parent().get_node("TestCube").set_translation(hit_pos)
-		
-		var up = Input.is_mouse_button_pressed(BUTTON_LEFT)
-		var down = Input.is_mouse_button_pressed(BUTTON_RIGHT)
-		
-		if up or down:
-			var cell_pos = world_to_cell_pos(hit_pos)
-			if Input.is_key_pressed(KEY_X):
-				var factor = 4.0*delta
-				_smooth(cell_pos.x, cell_pos.y, factor)
-			else:
-				var factor = 20.0 * delta
-				if down:
-					factor = -factor
-				_paint(cell_pos.x, cell_pos.y, factor)
+	var camera = get_viewport().get_camera()
+	
+	if camera != null:
+		var origin = camera.get_translation()
+		var dir = camera.get_transform().basis * Vector3(0,0,-1)
+		var hit_pos = raycast(origin, dir)
+		if hit_pos != null:
+			get_parent().get_node("TestCube").set_translation(hit_pos)
+			
+			var up = Input.is_mouse_button_pressed(BUTTON_LEFT)
+			var down = Input.is_mouse_button_pressed(BUTTON_RIGHT)
+			
+			if up or down:
+				var cell_pos = world_to_cell_pos(hit_pos)
+				if Input.is_key_pressed(KEY_X):
+					var factor = 4.0*delta
+					_smooth(cell_pos.x, cell_pos.y, factor)
+				else:
+					var factor = 20.0 * delta
+					if down:
+						factor = -factor
+					_paint(cell_pos.x, cell_pos.y, factor)
 	
 	_update_dirty_chunks()
 
@@ -169,19 +210,22 @@ func world_to_cell_pos(wpos):
 
 
 func cell_pos_is_valid(x, y):
-	return x >= 0 and y >= 0 and x < terrain_size and y < terrain_size
+	return x >= 0 and y >= 0 and x <= terrain_size and y <= terrain_size
 
 
-func generate_terrain():
-	for y in range(_data.size()):
-		var row = _data[y]
-		for x in range(row.size()):
-			row[x] = 2.0 * (cos(x*0.2) + sin(y*0.2))
+#func generate_terrain():
+#	for y in range(_data.size()):
+#		var row = _data[y]
+#		for x in range(row.size()):
+#			row[x] = 2.0 * (cos(x*0.2) + sin(y*0.2))
 
 
 func update_chunk_at(cx, cy):
 	var chunk = _chunks[cy][cx]
-	var mesh = generate_mesh_at(cx * CHUNK_SIZE, cy * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
+	update_chunk(chunk)
+
+func update_chunk(chunk):
+	var mesh = generate_mesh_at(chunk.pos.x * CHUNK_SIZE, chunk.pos.y * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
 	chunk.mesh_instance.set_mesh(mesh)
 
 
@@ -191,15 +235,17 @@ func generate_mesh_at(x0, y0, w, h):
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	#st.add_smooth_group(true)
 	
+	#print("Updating normals data (" + str(x0) + ", " + str(y0) + ", " + str(w) + ", " + str(h) + ")")
+	#_debug_print_actual_size(_normals, "normals")
 	update_normals_data_at(x0, y0, w, h)
 	
 	var max_y = y0 + w
 	var max_x = x0 + h
 	
-	if max_y+1 >= terrain_size:
-		max_y = terrain_size - 2
-	if max_x+1 >= terrain_size:
-		max_x = terrain_size - 2
+	if max_y >= terrain_size:
+		max_y = terrain_size
+	if max_x >= terrain_size:
+		max_x = terrain_size
 	
 	for y in range(y0, max_y):
 		var row = _data[y]
@@ -278,6 +324,10 @@ func _calculate_normal_at(x, y):
 	return Vector3(left - right, 2.0, fore - back).normalized()
 
 func update_normals_data_at(x0, y0, w, h):
+	if x0 + w > terrain_size:
+		w = terrain_size - x0
+	if y0 + h > terrain_size:
+		h = terrain_size - y0
 	var max_x = x0+w
 	var max_y = y0+h
 	for y in range(y0, max_y):
@@ -316,15 +366,96 @@ func _set_brush_radius(r):
 		print("Change brush radius " + str(r))
 
 
+#------------------------------------------------------------------
+# Helpers
+#------------------------------------------------------------------
+
+static func up_div(a, b):
+	if a % b != 0:
+		return a / b + 1
+	return a / b
+
+
 static func create_grid(w, h, v=null):
+	var is_create_func = typeof(v) == TYPE_OBJECT and v extends FuncRef
 	var grid = []
 	grid.resize(h)
 	for y in range(grid.size()):
 		var row = []
 		row.resize(w)
-		for x in range(row.size()):
-			row[x] = v
+		if is_create_func:
+			for x in range(row.size()):
+				row[x] = v(x,y)
+		else:
+			for x in range(row.size()):
+				row[x] = v
 		grid[y] = row
 	return grid
+
+# Resizes a 2D array and allows to set or call functions for each deleted and created cells
+static func resize_grid(grid, new_width, new_height, create_func=null, delete_func=null):
+	# Check parameters
+	assert(new_width >= 0 and new_height >= 0)
+	assert(grid != null)
+	if delete_func != null:
+		assert(typeof(delete_func) == TYPE_OBJECT and delete_func extends FuncRef)
+	var is_create_func = typeof(create_func) == TYPE_OBJECT and create_func extends FuncRef
+
+	# Get old size (supposed to be rectangular!)
+	var old_height = grid.size()
+	var old_width = 0
+	if grid.size() != 0:
+		old_width = grid[0].size()
+	
+	# Delete old rows
+	if new_height < old_height:
+		if delete_func != null:
+			for y in range(new_height, grid.size()):
+				var row = grid[y]
+				for x in range(0, row.size()):
+					var elem = row[x]
+					delete_func.call_func(elem)
+		grid.resize(new_height)
+	
+	# Delete old columns
+	if new_width < old_width:
+		for y in range(0, grid.size()):
+			var row = grid[y]
+			if delete_func != null:
+				for x in range(new_width, row.size()):
+					var elem = row[x]
+					delete_func.call_func(elem)
+			row.resize(new_width)
+	
+	# Create new columns
+	if new_width > old_width:
+		for y in range(0, grid.size()):
+			var row = grid[y]
+			row.resize(new_width)
+			if is_create_func:
+				for x in range(old_width, new_width):
+					row[x] = create_func.call_func(x,y)
+			else:
+				for x in range(old_width, new_width):
+					row[x] = create_func
+	
+	# Create new rows
+	if new_height > old_height:
+		grid.resize(new_height)
+		for y in range(old_height, new_height):
+			var row = []
+			row.resize(new_width)
+			grid[y] = row
+			if is_create_func:
+				for x in range(0, new_width):
+					row[x] = create_func.call_func(x,y)
+			else:
+				for x in range(0, new_width):
+					row[x] = create_func
+	
+	# Debug test check
+	assert(grid.size() == new_height)
+	for y in range(0, grid.size()):
+		assert(grid[y].size() == new_width)
 
 
