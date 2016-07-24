97f87e924e6c9593a60cb31fc24481d8476609c3
Marc Gilleron <marc.gilleron@gmail.com>
2016-07-25 00:53:12 +0200

    Separated helpers in another script file

--- terrain.gd
+++ terrain.gd
@@ -4,6 +4,8 @@ extends Node
 const CHUNK_SIZE = 16
 const MAX_TERRAIN_SIZE = 1024
 
+const Util = preload("terrain_utils.gd")
+
 class Chunk:
 	var mesh_instance = null
 	var pos = Vector2(0,0)
@@ -54,14 +56,14 @@ func set_terrain_size(new_size):
 
 
 func _on_terrain_size_changed():
-	_chunks_x = up_div(terrain_size, CHUNK_SIZE)
-	_chunks_y = up_div(terrain_size, CHUNK_SIZE)
+	_chunks_x = Util.up_div(terrain_size, CHUNK_SIZE)
+	_chunks_y = Util.up_div(terrain_size, CHUNK_SIZE)
 	
 	if is_inside_tree():
 		
-		resize_grid(_data, terrain_size+1, terrain_size+1, 0)
-		resize_grid(_normals, terrain_size+1, terrain_size+1, Vector3(0,1,0))
-		resize_grid(_chunks, _chunks_x, _chunks_y, funcref(self, "_create_chunk_cb"), funcref(self, "_delete_chunk_cb"))
+		Util.resize_grid(_data, terrain_size+1, terrain_size+1, 0)
+		Util.resize_grid(_normals, terrain_size+1, terrain_size+1, Vector3(0,1,0))
+		Util.resize_grid(_chunks, _chunks_x, _chunks_y, funcref(self, "_create_chunk_cb"), funcref(self, "_delete_chunk_cb"))
 		
 		for key in _dirty_chunks.keys():
 			if key.mesh_instance == null:
@@ -100,7 +102,7 @@ func _create_chunk_cb(x, y):
 
 func _generate_brush(radius):
 	var size = 2*radius
-	_brush = create_grid(size, size, 0)
+	_brush = Util.create_grid(size, size, 0)
 	_brush_sum = 0
 	for y in range(-radius, radius):
 		for x in range(-radius, radius):
@@ -364,98 +366,5 @@ func _set_brush_radius(r):
 		_brush_radius = r
 		_generate_brush(r)
 		print("Change brush radius " + str(r))
-
-
-#------------------------------------------------------------------
-# Helpers
-#------------------------------------------------------------------
-
-static func up_div(a, b):
-	if a % b != 0:
-		return a / b + 1
-	return a / b
-
-
-static func create_grid(w, h, v=null):
-	var is_create_func = typeof(v) == TYPE_OBJECT and v extends FuncRef
-	var grid = []
-	grid.resize(h)
-	for y in range(grid.size()):
-		var row = []
-		row.resize(w)
-		if is_create_func:
-			for x in range(row.size()):
-				row[x] = v(x,y)
-		else:
-			for x in range(row.size()):
-				row[x] = v
-		grid[y] = row
-	return grid
-
-# Resizes a 2D array and allows to set or call functions for each deleted and created cells
-static func resize_grid(grid, new_width, new_height, create_func=null, delete_func=null):
-	# Check parameters
-	assert(new_width >= 0 and new_height >= 0)
-	assert(grid != null)
-	if delete_func != null:
-		assert(typeof(delete_func) == TYPE_OBJECT and delete_func extends FuncRef)
-	var is_create_func = typeof(create_func) == TYPE_OBJECT and create_func extends FuncRef
-
-	# Get old size (supposed to be rectangular!)
-	var old_height = grid.size()
-	var old_width = 0
-	if grid.size() != 0:
-		old_width = grid[0].size()
-	
-	# Delete old rows
-	if new_height < old_height:
-		if delete_func != null:
-			for y in range(new_height, grid.size()):
-				var row = grid[y]
-				for x in range(0, row.size()):
-					var elem = row[x]
-					delete_func.call_func(elem)
-		grid.resize(new_height)
-	
-	# Delete old columns
-	if new_width < old_width:
-		for y in range(0, grid.size()):
-			var row = grid[y]
-			if delete_func != null:
-				for x in range(new_width, row.size()):
-					var elem = row[x]
-					delete_func.call_func(elem)
-			row.resize(new_width)
-	
-	# Create new columns
-	if new_width > old_width:
-		for y in range(0, grid.size()):
-			var row = grid[y]
-			row.resize(new_width)
-			if is_create_func:
-				for x in range(old_width, new_width):
-					row[x] = create_func.call_func(x,y)
-			else:
-				for x in range(old_width, new_width):
-					row[x] = create_func
-	
-	# Create new rows
-	if new_height > old_height:
-		grid.resize(new_height)
-		for y in range(old_height, new_height):
-			var row = []
-			row.resize(new_width)
-			grid[y] = row
-			if is_create_func:
-				for x in range(0, new_width):
-					row[x] = create_func.call_func(x,y)
-			else:
-				for x in range(0, new_width):
-					row[x] = create_func
-	
-	# Debug test check
-	assert(grid.size() == new_height)
-	for y in range(0, grid.size()):
-		assert(grid[y].size() == new_width)
 
 
---
+++ terrain_utils.gd
@@ -0,0 +1,92 @@
+
+# Performs a positive integer division rounded to upper (4/2 = 2, 5/3 = 2)
+static func up_div(a, b):
+	if a % b != 0:
+		return a / b + 1
+	return a / b
+
+# Creates a 2D array as an array of arrays
+static func create_grid(w, h, v=null):
+	var is_create_func = typeof(v) == TYPE_OBJECT and v extends FuncRef
+	var grid = []
+	grid.resize(h)
+	for y in range(grid.size()):
+		var row = []
+		row.resize(w)
+		if is_create_func:
+			for x in range(row.size()):
+				row[x] = v(x,y)
+		else:
+			for x in range(row.size()):
+				row[x] = v
+		grid[y] = row
+	return grid
+
+# Resizes a 2D array and allows to set or call functions for each deleted and created cells.
+# This is especially useful if cells contain objects and you don't want to loose existing data.
+static func resize_grid(grid, new_width, new_height, create_func=null, delete_func=null):
+	# Check parameters
+	assert(new_width >= 0 and new_height >= 0)
+	assert(grid != null)
+	if delete_func != null:
+		assert(typeof(delete_func) == TYPE_OBJECT and delete_func extends FuncRef)
+	var is_create_func = typeof(create_func) == TYPE_OBJECT and create_func extends FuncRef
+
+	# Get old size (supposed to be rectangular!)
+	var old_height = grid.size()
+	var old_width = 0
+	if grid.size() != 0:
+		old_width = grid[0].size()
+	
+	# Delete old rows
+	if new_height < old_height:
+		if delete_func != null:
+			for y in range(new_height, grid.size()):
+				var row = grid[y]
+				for x in range(0, row.size()):
+					var elem = row[x]
+					delete_func.call_func(elem)
+		grid.resize(new_height)
+	
+	# Delete old columns
+	if new_width < old_width:
+		for y in range(0, grid.size()):
+			var row = grid[y]
+			if delete_func != null:
+				for x in range(new_width, row.size()):
+					var elem = row[x]
+					delete_func.call_func(elem)
+			row.resize(new_width)
+	
+	# Create new columns
+	if new_width > old_width:
+		for y in range(0, grid.size()):
+			var row = grid[y]
+			row.resize(new_width)
+			if is_create_func:
+				for x in range(old_width, new_width):
+					row[x] = create_func.call_func(x,y)
+			else:
+				for x in range(old_width, new_width):
+					row[x] = create_func
+	
+	# Create new rows
+	if new_height > old_height:
+		grid.resize(new_height)
+		for y in range(old_height, new_height):
+			var row = []
+			row.resize(new_width)
+			grid[y] = row
+			if is_create_func:
+				for x in range(0, new_width):
+					row[x] = create_func.call_func(x,y)
+			else:
+				for x in range(0, new_width):
+					row[x] = create_func
+	
+	# Debug test check
+	assert(grid.size() == new_height)
+	for y in range(0, grid.size()):
+		assert(grid[y].size() == new_width)
+
+
