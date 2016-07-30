a15675073dcf71006c2984821be95b2af9f1b6c9
Marc Gilleron <marc.gilleron@gmail.com>
2016-07-30 13:22:55 +0200

    Added a menu to choose the paint mode

--- addons/terrain/terrain_plugin.gd
+++ addons/terrain/terrain_plugin.gd
@@ -7,14 +7,51 @@ const Terrain = preload("res://terrain.gd")
 var current_object = null
 
 var _pressed = false
+var _mode_selector = null
+var _paint_mode = Terrain.PAINT_MODE_ADD
 
 
 func _enter_tree():
 	add_custom_type(TARGET_TYPE, "Node", Terrain, preload("icon.png"))
+	
+	_mode_selector = MenuButton.new()
+	_mode_selector.set_text("Terrain")
+	var popup = _mode_selector.get_popup()
+	popup.add_check_item("Add", Terrain.PAINT_MODE_ADD)
+	popup.add_check_item("Subtract", Terrain.PAINT_MODE_SUBTRACT)
+	popup.add_check_item("Smooth", Terrain.PAINT_MODE_SMOOTH)
+	popup.set_item_checked(_paint_mode, true)
+	popup.connect("item_pressed", self, "_on_mode_selector_item_pressed")
+	add_control_to_container(CONTAINER_SPATIAL_EDITOR_MENU, _mode_selector)
+	_mode_selector.hide()
+	
+	var selection = get_selection()
+	selection.connect("selection_changed", self, "_on_selection_changed")
+	_on_selection_changed()
 
 
 func _exit_tree():
+	var selection = get_selection()
+	selection.disconnect("selection_changed", self, "_on_selection_changed")
+	
+	_mode_selector.free()
+	_mode_selector = null
+	
 	remove_custom_type(TARGET_TYPE)
+
+
+func _on_mode_selector_item_pressed(item_id):
+	var popup = _mode_selector.get_popup()
+	popup.set_item_checked(_paint_mode, false)
+	popup.set_item_checked(item_id, true)
+	_paint_mode = item_id
+
+
+func _on_selection_changed():
+	if current_object != null:
+		var selected_nodes = get_selection().get_selected_nodes()
+		if selected_nodes.size() != 1 or not handles(selected_nodes[0]):
+			stop_edit()
 
 
 func paint(camera, mouse_pos, mode):
@@ -27,14 +64,16 @@ func paint(camera, mouse_pos, mode):
 
 
 func handles(object):
-	print("Handles " + str(object.get_type()))
-	#return object.is_type(TARGET_TYPE)
 	return object extends Terrain
 
-# TODO Use the selection in Godot 2.1
+
 func edit(object):
-	#print("edit(" + str(object) + ")")
 	current_object = object
+	_mode_selector.show()
+
+func stop_edit():
+	current_object = null
+	_mode_selector.hide()
 
 
 func forward_spatial_input_event(camera, event):
@@ -50,12 +89,12 @@ func forward_spatial_input_event(camera, event):
 	
 	elif _pressed and event.type == InputEvent.MOUSE_MOTION:
 		
-		if Input.is_mouse_button_pressed(BUTTON_LEFT):
-			paint(camera, event.pos, Terrain.PAINT_MODE_ADD)
-			captured_event = true
-			
-		elif Input.is_mouse_button_pressed(BUTTON_RIGHT):
+		if _paint_mode == Terrain.PAINT_MODE_ADD and Input.is_mouse_button_pressed(BUTTON_RIGHT):
 			paint(camera, event.pos, Terrain.PAINT_MODE_SUBTRACT)
+			captured_event = true
+		
+		elif Input.is_mouse_button_pressed(BUTTON_LEFT):
+			paint(camera, event.pos, _paint_mode)
 			captured_event = true
 	
 	return captured_event
